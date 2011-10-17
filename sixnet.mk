e0805a352bd8db06120ac553b43806176a77d9cf
Matthew Caron <Matt.Caron@sixnet.com>
2011-10-17 14:57:47 -0400

    Added sixnet.mk. Refs #8023

---
+++ sixnet.mk
@@ -0,0 +1,26 @@
+export BUILD_DIR ?= $(PWD)
+include $(BUILD_DIR)/rules/common.mk
+
+COMPONENT := tiobench
+
+STAGING_DIR ?= $(STAGING)/$(COMPONENT)
+
+CONFIGURE_OPT = \
+	$(CONFIGURE_OPT_$(ARCH))
+
+$(shell echo "**** Building $(COMPONENT) for $(PROD) ****" >&2)
+
+.PHONY: all build clean install
+
+all: install
+
+build:
+	$(MAKE) -j $(MAKE_JOBS)
+
+clean:
+	$(MAKE) clean
+
+install: build
+	-rm -fr $(STAGING_DIR)
+	mkdir -p $(STAGING_DIR)
+	$(MAKE) DESTDIR=$(STAGING_DIR) install
